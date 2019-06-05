7bf2089adf01fc2f371b7b5a31c87522a9f8f2c7
Varis Darasirikul <varis.dara@gmail.com>
2019-06-06 01:31:57 +0800

    Update nodejs config server

--- nodejs/src/scripts/app.ts
+++ nodejs/src/scripts/app.ts
@@ -2,7 +2,6 @@ import * as Koa from 'koa'
 import * as serve from 'koa-static'
 
 const app = new Koa();
-
-app.use(serve("public", {maxAge: 10 * 60 * 60}))
+app.use(serve("../public", {maxAge: 10 * 60 * 60}))
 
 export default app
