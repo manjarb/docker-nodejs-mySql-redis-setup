dcc58db89ac1939cd2a3f5772294d9bac9ed2f72
Varis Darasirikul <varis.dara@gmail.com>
2019-06-06 18:04:19 +0800

    Add database connect

--- nodejs/src/scripts/app.ts
+++ nodejs/src/scripts/app.ts
@@ -1,7 +1,9 @@
 import * as Koa from 'koa'
 import * as serve from 'koa-static'
+import {dbConnect} from '../model/sequelize'
 
 const app = new Koa();
 app.use(serve("public", {maxAge: 10 * 60 * 60}))
+dbConnect();
 
 export default app
