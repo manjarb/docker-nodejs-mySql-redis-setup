c3e17cbedc70983f123c7b6810b2df3f5e61a27f
Varis Darasirikul <varis.dara@gmail.com>
2019-06-06 17:21:52 +0800

    Add sequelize

--- nodejs/src/config/config.local.ts
+++ nodejs/src/config/config.local.ts
@@ -5,5 +5,20 @@ export default {
     proxyServer: {
         host: '',
         port: 80
-    }
+    },
+    db: {
+        dialect: "mysql",
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: '',
+        database: 'RAP2_DELOS_APP_LOCAL',
+        pool: {
+            max: 100,
+            min: 0,
+            idle: 10000,
+        },
+        logging: true
+    },
+    redis: {}
 }
